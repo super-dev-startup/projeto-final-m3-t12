905ede1e9f41d3af343f4c0278969f999578954c
Nicole Antunes <102484958+nicoleantuness@users.noreply.github.com>
2022-09-03 11:01:25 -0300

    finish apresentation page

--- src/pages/ApresentationPage/index.jsx
+++ src/pages/ApresentationPage/index.jsx
@@ -11,31 +11,32 @@ function ApresentationPage() {
         api
         .get(`/apresentation`)
         .then((response) => {
-          setUser(response);
+            console.log(response.data.subtitle)
+          setUser(response.data);
         })
         .catch((error) => {
           console.log(error);
         });
   }, []);
-  console.log(user)
+
     return(
         <>
         <Main>
             <StyledIntro>
                 <div className="name">
-                    <h3>Tyler</h3>
+                    <h3>TYLER</h3>
                     <img src={Vector} alt="logo" />
-                    <h3>Barthold</h3>
+                    <h3>BARTHOLD</h3>
                 </div>
                 <h1>Best creative and modern fotographer.</h1>
             </StyledIntro>
             <article>
                 <StyledText>
                     <h2>
-                    {user.data.subtitle}
+                    {user.title}
                     </h2>
                     <p>
-                    {user.data.body}
+                        {user.subtitle}
                     </p>
                 </StyledText>
                 <StyledWrapperInfo>
@@ -45,24 +46,24 @@ function ApresentationPage() {
                         <p>Date of birth</p>
                     </div>
                     <div className="Info">
-                        <p>New York</p>
-                        <p>{user.data.experience}</p>
-                        <p>27 june 1992</p>
+                        <p>{user["born-in"]}</p>
+                        <p>{user.experience}</p>
+                        <p>{user["date-of-birth"]}</p>
                     </div>
                 </StyledWrapperInfo>
             </article>
             </Main>
             <Section>
                 <div className="StledContainer">
-                    <h1>{user.data.experience}</h1>
+                    <h1>{user.experience}</h1>
                     <p>YEARS OF EXPERIENCE</p>
                 </div>
                 <div className="StledContainerProject">
-                    <h1>50+</h1>
+                    <h1>{user["project-completed"]}</h1>
                     <p>PROJECT COMPLETED</p>
                 </div>
                 <div className="StledContainer">
-                    <h1>30+</h1>
+                    <h1>{user["happy-cliente"]}</h1>
                     <p>HAPPY CLIENTS</p>
                 </div>
             </Section>
