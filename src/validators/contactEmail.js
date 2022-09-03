8e886644dba43416513a7c2dbaeea2434c7067a4
4lisson777 <alissonlima529@gmail.com>
2022-09-03 09:54:33 -0300

    feat: validação do form com yup

---
+++ src/validators/contactEmail.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+import * as yup from 'yup';
+
+export const contactEmail = yup.object({
+  firstName: yup.string('Apenas texto!').required('Campo obrigatório'),
+  lastName: yup.string('Apenas texto!').required('Campo obrigatório'),
+  email: yup
+    .string()
+    .email('Formato de email inválido!')
+    .required('Email obrigatório!'),
+  phone: yup.string().required('Campo obrigatório!'),
+  message: yup.string('Apenas texto!').required('Campo obrigatório'),
+});
