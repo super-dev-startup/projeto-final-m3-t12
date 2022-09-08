96f56d4b94e98d7de410fec15b493b8fc5778513
4lisson777 <alissonlima529@gmail.com>
2022-09-08 03:26:18 -0300

    style: adicionado responsividade na pagina de apresentacao

--- src/pages/ApresentationPage/index.jsx
+++ src/pages/ApresentationPage/index.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import api from "../../api/api";
+import { useState, useEffect } from 'react';
+import api from '../../api/api';
 
 import {
   Main,
@@ -7,8 +7,8 @@ import {
   StyledText,
   StyledWrapperInfo,
   Section,
-} from "./style";
-import Vector from "../../imgs/Vector.png";
+} from './style';
+import moonSmall from '../../assets/moonSmall.svg';
 
 function ApresentationPage() {
   const [user, setUser] = useState({});
@@ -28,7 +28,7 @@ function ApresentationPage() {
         <StyledIntro>
           <div className="name">
             <h3>TYLER</h3>
-            <img src={Vector} alt="logo" />
+            <img src={moonSmall} alt="logo" />
             <h3>BARTHOLD</h3>
           </div>
           <h1>Best creative and modern fotographer.</h1>
@@ -45,9 +45,9 @@ function ApresentationPage() {
               <p>Date of birth</p>
             </div>
             <div className="Info">
-              <p>{user["born-in"]}</p>
+              <p>{user['born-in']}</p>
               <p>{user.experience}</p>
-              <p>{user["date-of-birth"]}</p>
+              <p>{user['date-of-birth']}</p>
             </div>
           </StyledWrapperInfo>
         </article>
@@ -58,11 +58,11 @@ function ApresentationPage() {
           <p>YEARS OF EXPERIENCE</p>
         </div>
         <div className="StledContainerProject">
-          <h1>{user["project-completed"]}</h1>
+          <h1>{user['project-completed']}</h1>
           <p>PROJECT COMPLETED</p>
         </div>
         <div className="StledContainer">
-          <h1>{user["happy-cliente"]}</h1>
+          <h1>{user['happy-cliente']}</h1>
           <p>HAPPY CLIENTS</p>
         </div>
       </Section>
